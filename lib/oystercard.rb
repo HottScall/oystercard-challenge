c338d7649a4d3ef9634499b9834b9f3b0a5d7f43
Scott <Scotthall1983@hotmail.com>
2019-04-10 16:44:26 +0100

    Challenge 8 - Touch In & Out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,10 +2,13 @@ class Oystercard
 
   MAXIMUM_BALANCE = 90
 
+
+
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -14,7 +17,19 @@ class Oystercard
   end
 
   def deduct(amount)
-    @balance -= amount 
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false 
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -22,4 +22,26 @@ describe Oystercard do
       expect{ subject.deduct 1 }.to change{ subject.balance }.by (-1)
     end
   end
+
+  describe "#in_journey?" do
+    it "returns false for a new card" do
+      expect(subject).not_to be_in_journey
+    end
+  end
+
+  describe "#touch_in" do
+    it "change in_journey from false to true" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+      # expect{subject.touch_in}.to change{subject.in_journey?}.to(true)
+    end
+  end
+
+  describe "#touch_out" do
+    it "change journey from true to false" do
+      subject.touch_in    
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
 end
