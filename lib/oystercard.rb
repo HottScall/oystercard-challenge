b820ced9bfd39f38d52287b9413ccf0d8c1f436f
Scott <Scotthall1983@hotmail.com>
2019-04-11 16:49:44 +0100

    Challenge 10 & 11 - Charging & Entry Station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,35 +2,46 @@ class Oystercard
 
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
+  FARE = 1
 
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   def initialize
     @balance = 0
-    @in_journey = false
+    # @in_journey = false
   end
+
 
   def top_up(amount)
     fail "Maximum Balance of #{MAXIMUM_BALANCE} Exceeded" if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
   def in_journey?
-    @in_journey
+    !!entry_station
+    # @in_journey
   end
 
-  def touch_in
+  def touch_in(station)
+    @entry_station = station
     raise "Minimum balance is £#{MINIMUM_BALANCE}" if @balance < MINIMUM_BALANCE
-    @in_journey = true
+    # @in_journey = true
+
+  in_journey?
   end
 
   def touch_out
-    @in_journey = false
+    deduct(FARE)
+    @entry_station = nil
+    in_journey?
+    # @in_journey = false
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station){double :Entry}
   it "is expected to have a balance of zero" do
     expect(subject.balance).to eq(0)
   end
@@ -17,38 +18,53 @@ describe Oystercard do
     expect{ subject.top_up 1 }.to raise_error "Maximum Balance of #{maximum_balance} Exceeded"
   end
 
-  describe "#deduct" do
-    it "deducts money from balance when used" do
-      expect{ subject.deduct 1 }.to change{ subject.balance }.by (-1)
-    end
-  end
+  # describe "#deduct" do
+  #   it "deducts money from balance when used" do
+  #     expect{ subject.deduct 1 }.to change{ subject.balance }.by (-1)
+  #   end
+  # end
 
   describe "#in_journey?" do
     it "returns false for a new card" do
       expect(subject).not_to be_in_journey
+    end
+    it "returns true when in journey" do
+      subject.top_up(1)
+      subject.touch_in(station)
+      expect(subject.in_journey?).to eq(true)
     end
   end
 
   describe "#touch_in" do
     it "change in_journey from false to true" do
       subject.top_up(1)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject).to be_in_journey
-      # expect{subject.touch_in}.to change{subject.in_journey?}.to(true)
+      #expect{subject.touch_in}.to change{subject.in_journey?}.to(true)
+    end
+
+    it "logs the stations upon tap in" do
+      subject.top_up(1)
+      subject.touch_in(station)
+    expect(subject.entry_station).to eq(station)
     end
 
     it "rejects journey if current balance is less than minimum fare" do
       minimum_balance = Oystercard::MINIMUM_BALANCE
-      expect{subject.touch_in}.to raise_error("Minimum balance is £#{minimum_balance}")
+      expect{subject.touch_in(station)}.to raise_error("Minimum balance is £#{minimum_balance}")
     end
   end
 
   describe "#touch_out" do
     it "change journey from true to false" do
       subject.top_up(1)
-      subject.touch_in
+      subject.touch_in(station)
       subject.touch_out
       expect(subject).not_to be_in_journey
     end
+    it "deducts the fare upon touch_out" do
+      expect{subject.touch_out}.to change{subject.balance}.by(-1)
+    end
   end
+
 end
